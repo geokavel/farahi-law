fa0a660f897cb3e40bc27f9915f5d4cea6d02551
George Kaveladze <geokavel@Georges-MacBook-Pro.local>
2018-04-13 12:49:31 -0700

    Setup Videos carousel and Latest Articles section

---
+++ videos.js
@@ -0,0 +1,6 @@
+var videos = [
+{vidId:"faQUwLFD3P8",desc:"About Farahi Law"},
+{vidId:"iQ3W_1kjdPk",desc:"Top 5 New Car Safety Considerations"},
+{vidId:"R8QDPeerzjE",desc:"Testimonial: Yan Skwar"},
+{vidId:"3r7V3vhlOLM",desc:"Recomendación: Flora Corona"},
+{vidId:"ZWV5E2ynT_s",desc:"Testimonial: Denise Romero"}];
